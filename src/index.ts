0f067d0147d87f35c6f1559e8cf71726d53c4e03
ToseebNadaf <toseebnadaf@gmail.com>
2024-12-12 23:35:45 +0530

    WS connected to start communication

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,44 @@
-import { WebSocketServer } from "ws";
+import { WebSocketServer, WebSocket } from "ws";
 
 const wss = new WebSocketServer({ port: 8080 });
 
+const subscriptions: {
+  [key: string]: {
+    ws: WebSocket;
+    rooms: string[];
+  };
+} = {};
+
 wss.on("connection", (socket) => {
-  socket.on("error", console.error);
+  const id = randomID();
+  subscriptions[id] = {
+    ws: socket,
+    rooms: [],
+  };
 
   socket.on("message", (data) => {
-    console.log("received: %s", data);
-    socket.send("Data --> " + data);
+    //@ts-ignore
+    const parsedMessage = JSON.parse(data);
+    if (parsedMessage.type === "SUBSCRIBE") {
+      subscriptions[id].rooms.push(parsedMessage.room);
+    } 
+
+    if (parsedMessage.type === "sendMessage") {
+      const message = parsedMessage.message;
+      const roomId = parsedMessage.roomId;
+
+      for (const userId in subscriptions) {
+        if (subscriptions.hasOwnProperty(userId)) {
+          const { ws, rooms } = subscriptions[userId];
+          if (rooms.includes(roomId)) {
+            ws.send(message);
+          }
+        }
+      }
+    }
   });
 });
+
+const randomID = () => {
+  return Math.random();
+};
