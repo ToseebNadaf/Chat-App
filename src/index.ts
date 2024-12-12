1e069275729ef2f221be505830d0f0d940c0a496
ToseebNadaf <toseebnadaf@gmail.com>
2024-12-13 00:39:23 +0530

    Completed whole flow using pub-sub

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,11 @@
 import { WebSocketServer, WebSocket } from "ws";
+import { createClient } from "redis";
+
+const publishClient = createClient();
+publishClient.connect();
+
+const subscribeClient = createClient();
+subscribeClient.connect();
 
 const wss = new WebSocketServer({ port: 8080 });
 
@@ -21,23 +28,75 @@ wss.on("connection", (socket) => {
     const parsedMessage = JSON.parse(data);
     if (parsedMessage.type === "SUBSCRIBE") {
       subscriptions[id].rooms.push(parsedMessage.room);
-    } 
+      if (oneUserSubscribedTo(parsedMessage.room)) {
+        subscribeClient.subscribe(parsedMessage.room, (message) => {
+          const parsedMessage = JSON.parse(message);
+          const roomId = parsedMessage.roomId;
+
+          for (const userId in subscriptions) {
+            if (subscriptions.hasOwnProperty(userId)) {
+              const { ws, rooms } = subscriptions[userId];
+              if (rooms.includes(parsedMessage.roomId)) {
+                ws.send(parsedMessage.message);
+              }
+            }
+          }
+        });
+      }
+    }
+
+    if (parsedMessage.type === "UNSUBSCRIBE") {
+      subscriptions[id].rooms = subscriptions[id].rooms.filter(
+        (x) => x !== parsedMessage.room
+      );
+      if (lastPersonLeftRoom(parsedMessage.room)) {
+        subscribeClient.unsubscribe(parsedMessage.room);
+      }
+    }
 
     if (parsedMessage.type === "sendMessage") {
       const message = parsedMessage.message;
       const roomId = parsedMessage.roomId;
 
-      for (const userId in subscriptions) {
-        if (subscriptions.hasOwnProperty(userId)) {
-          const { ws, rooms } = subscriptions[userId];
-          if (rooms.includes(roomId)) {
-            ws.send(message);
-          }
-        }
-      }
+      publishClient.publish(
+        roomId,
+        JSON.stringify({
+          type: "sendMessage",
+          roomId: roomId,
+          message,
+        })
+      );
     }
   });
 });
+
+const oneUserSubscribedTo = (roomId: string) => {
+  let totalInterestedPeople = 0;
+  for (const userId in subscriptions) {
+    if (subscriptions[userId].rooms.includes(roomId)) {
+      totalInterestedPeople++;
+    }
+  }
+
+  if (totalInterestedPeople == 1) {
+    return true
+  }
+  return false
+};
+
+const lastPersonLeftRoom = (roomId: string) => {
+  let totalInterestedPeople = 0;
+  for (const userId in subscriptions) {
+    if (subscriptions[userId].rooms.includes(roomId)) {
+      totalInterestedPeople++;
+    }
+  }
+
+  if (totalInterestedPeople == 0) {
+    return true
+  }
+  return false
+}
 
 const randomID = () => {
   return Math.random();
